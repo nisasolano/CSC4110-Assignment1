a99b5fcd7bdd25f5861c0d0f2d440fc749a35325
Nisa Solano <ho9288@wayne.edu>
2025-09-09 17:13:58 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,7 @@
+
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+//Author: Nisa Solano <ho9288@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
